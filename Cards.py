3c4c1fc2f8feebaff533b2070aea119bb4d1d4be
nitinks <nitinksharma.viet@gmail.com>
2020-06-14 21:25:21 +0530

    Update Cards.py

--- Cards.py
+++ Cards.py
@@ -2,14 +2,14 @@
 
 import collections
 
-Cards = collections.namedtuple('Card',['rank', 'suit'])
+Card = collections.namedtuple('Card',['rank', 'suit'])
 
 class FrenchDeck:
     ranks = [str(rank) for rank in range(2,11)] + list('JQKA')
     suit = 'spades diamonds clubs hearts'.split()
     
     def __init__(self):
-        self._cards = [ Card(rank, suit) for suit in self.suit 
+        self._cards = [Card(rank, suit) for suit in self.suit
                                          for rank in self.ranks]
                                          
     def __len__(self):
@@ -17,6 +17,48 @@ class FrenchDeck:
         
     def __getitem__(self, position):
         return self._cards[position]
+
+    def __contains__(self, item):
+        if item in self._cards:
+            return True
+        return False
+
+    def __str__(self):
+        return "Card Deck with {} cards from {} * {}".format(len(self), self.ranks, self.suit)
         
 Deck = FrenchDeck()
 print ("Total Cards in French Deck : {}".format(len(Deck)))
+
+# Get An Elements from the class.
+print("Second Card is : {}".format(Deck[2-1]))
+print("Twenty Second Card is : {}".format(Deck[22-1]))
+
+# Get a random Card. Use random.choice as FrenchDeck is a sequence.
+from random import choice
+print ("Random Card {}".format(choice(Deck)))
+
+# Slicing.
+print(Deck[:3])
+print(Deck[11:14])
+print(Deck[12::13])
+
+# presence check.
+card = Card('Q','hearts')
+print ("Card is :", card)
+print("Whether card in Deck: ",card in Deck)
+
+print (Deck)
+
+
+#  nsharma@nsharma-mbps ~/Documents/GitHub/RandomCodes $ python3 Cards.py
+# Total Cards in French Deck : 52
+# Second Card is : Card(rank='3', suit='spades')
+# Twenty Second Card is : Card(rank='10', suit='diamonds')
+# Random Card Card(rank='Q', suit='hearts')
+# [Card(rank='2', suit='spades'), Card(rank='3', suit='spades'), Card(rank='4', suit='spades')]
+# [Card(rank='K', suit='spades'), Card(rank='A', suit='spades'), Card(rank='2', suit='diamonds')]
+# [Card(rank='A', suit='spades'), Card(rank='A', suit='diamonds'), Card(rank='A', suit='clubs'), Card(rank='A', suit='hearts')]
+# Card is : Card(rank='Q', suit='hearts')
+# Whether card in Deck:  True
+# Card Deck with 52 cards from ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'] * ['spades', 'diamonds', 'clubs', 'hearts']
+#  nsharma@nsharma-mbps ~/Documents/GitHub/RandomCodes $
